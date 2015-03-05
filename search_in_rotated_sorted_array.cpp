2d7989b711767b87294aaba54354bcbc8c82b2a2
winterma <mahuaguan@126.com>
2015-03-06 01:54:38 +0800

    search

--- search_in_rotated_sorted_array.cpp
+++ search_in_rotated_sorted_array.cpp
@@ -1,4 +1,7 @@
 #include <vector>
+#include <iostream>
+
+using namespace std;
 
 class Solution {
 public:
@@ -9,7 +12,41 @@ public:
 
         while(left <= right)
         {
+            mid = left + (right - left) / 2;
 
+            if(A[mid] == target) return mid;
+
+            else if(A[mid] > target)
+            {
+                if(A[left] > target && A[left] <= A[mid]) left = mid + 1;
+                else right = mid - 1;
+            }
+
+            else
+            {
+                if(A[left] <= A[mid]) left = mid + 1;
+                else if(A[left] > target) left = mid + 1;
+                else if(A[left] <= target) right = mid - 1;
+                
+                
+            }
         }
+
+        return -1;
     }
 };
+
+
+
+int main()
+{
+    Solution a;
+
+    int data[2];
+    int target;
+    cin >> data[0] >> data[1] >> target;
+
+    int res = a.search(data, 2, target);
+    cout << res << endl;
+    return 0;
+}
