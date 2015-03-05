9819551dd8de9a1ea787f0ab568ac9bb06771074
winterma <mahuaguan@126.com>
2015-03-06 00:25:28 +0800

    bc

---
+++ search_in_rotated_sorted_array.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(int A[], int n, int target) {
+        int left = 0;
+        int right = n - 1;
+        int mid;
+
+        while(left <= right)
+        {
+
+        }
+    }
+};
